48e2b79607772a07faf634ece7b5e70d360f4173
mingyuShim94 <asdgfhgjk90@gmail.com>
2024-02-06 23:44:01 +0900

    home_dialog_design

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -48,16 +48,92 @@ class Home extends StatelessWidget {
                     const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
                 child: Column(
                   children: [
-                    Container(
-                      width: double.infinity,
-                      height: 70,
-                      color: Colors.amber,
-                      child: const Center(
-                        child: Text(
-                          '오늘 감사한 것을 적어보세요.',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
+                    GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              contentPadding: const EdgeInsets.all(0),
+                              content: Container(
+                                width: 400,
+                                height: 400,
+                                decoration:
+                                    const BoxDecoration(color: Colors.green),
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: Container(
+                                          alignment: Alignment.centerLeft,
+                                          color: Colors.purpleAccent,
+                                          child: const Text('감사노트 작성하기',
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                color: Colors.white,
+                                              ))),
+                                    ),
+                                    Expanded(
+                                      flex: 5,
+                                      child: Container(
+                                        color: Colors.yellow,
+                                        child: const TextField(
+                                          maxLines: 7,
+                                          decoration: InputDecoration(
+                                            border: OutlineInputBorder(),
+                                            labelText: '감사한 것을 적어주세요.',
+                                            alignLabelWithHint: true,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 2,
+                                      child: Container(
+                                        color: Colors.tealAccent,
+                                        child: const TextField(
+                                          maxLines: 7,
+                                          decoration: InputDecoration(
+                                            border: OutlineInputBorder(),
+                                            labelText: '감사한 대상을 적어주세요.',
+                                            alignLabelWithHint: true,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: Container(
+                                        color: Colors.lightGreenAccent,
+                                        child: ElevatedButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text(
+                                            '저장하기',
+                                            style: TextStyle(fontSize: 18),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        height: 70,
+                        color: Colors.amber,
+                        child: const Center(
+                          child: Text(
+                            '오늘 감사한 것을 적어보세요.',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
