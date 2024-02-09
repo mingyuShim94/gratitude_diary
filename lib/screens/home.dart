29225641a34bf0d529fa6852969cb6057e8e94a1
mingyuShim94 <asdgfhgjk90@gmail.com>
2024-02-09 10:18:30 +0900

    note_textform

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final GlobalKey<FormState> formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -65,39 +72,89 @@ class Home extends StatelessWidget {
                                     Expanded(
                                       flex: 1,
                                       child: Container(
-                                          alignment: Alignment.centerLeft,
-                                          color: Colors.purpleAccent,
-                                          child: const Text('감사노트 작성하기',
+                                        alignment: Alignment.centerLeft,
+                                        color: Colors.purpleAccent,
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            const Text(
+                                              '감사노트 작성하기',
                                               style: TextStyle(
                                                 fontSize: 20,
                                                 color: Colors.white,
-                                              ))),
-                                    ),
-                                    Expanded(
-                                      flex: 5,
-                                      child: Container(
-                                        color: Colors.yellow,
-                                        child: const TextField(
-                                          maxLines: 7,
-                                          decoration: InputDecoration(
-                                            border: OutlineInputBorder(),
-                                            labelText: '감사한 것을 적어주세요.',
-                                            alignLabelWithHint: true,
-                                          ),
+                                              ),
+                                            ),
+                                            IconButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              icon: const Icon(
+                                                Icons.close,
+                                                color: Colors.white,
+                                              ),
+                                            )
+                                          ],
                                         ),
                                       ),
                                     ),
                                     Expanded(
-                                      flex: 2,
-                                      child: Container(
-                                        color: Colors.tealAccent,
-                                        child: const TextField(
-                                          maxLines: 7,
-                                          decoration: InputDecoration(
-                                            border: OutlineInputBorder(),
-                                            labelText: '감사한 대상을 적어주세요.',
-                                            alignLabelWithHint: true,
-                                          ),
+                                      flex: 7,
+                                      child: Form(
+                                        key: formKey,
+                                        child: Column(
+                                          children: [
+                                            Expanded(
+                                              flex: 5,
+                                              child: Container(
+                                                padding:
+                                                    const EdgeInsets.all(10),
+                                                color: Colors.yellow,
+                                                child: TextFormField(
+                                                  validator: (value) {
+                                                    if (value == null ||
+                                                        value.isEmpty) {
+                                                      return '내용을 입력해주세요.';
+                                                    }
+                                                    return null;
+                                                  },
+                                                  maxLines: 7,
+                                                  decoration:
+                                                      const InputDecoration(
+                                                    border:
+                                                        OutlineInputBorder(),
+                                                    labelText: '감사한 것을 적어주세요.',
+                                                    alignLabelWithHint: true,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            Expanded(
+                                              flex: 2,
+                                              child: Container(
+                                                padding:
+                                                    const EdgeInsets.all(10),
+                                                color: Colors.tealAccent,
+                                                child: TextFormField(
+                                                  validator: (value) {
+                                                    if (value == null ||
+                                                        value.isEmpty) {
+                                                      return '내용을 입력해주세요.';
+                                                    }
+                                                    return null;
+                                                  },
+                                                  maxLines: 7,
+                                                  decoration:
+                                                      const InputDecoration(
+                                                    border:
+                                                        OutlineInputBorder(),
+                                                    labelText: '감사한 대상을 적어주세요.',
+                                                    alignLabelWithHint: true,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ),
                                     ),
@@ -107,7 +164,7 @@ class Home extends StatelessWidget {
                                         color: Colors.lightGreenAccent,
                                         child: ElevatedButton(
                                           onPressed: () {
-                                            Navigator.pop(context);
+                                            savePressed();
                                           },
                                           child: const Text(
                                             '저장하기',
@@ -161,5 +218,18 @@ class Home extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void savePressed() {
+    //formKey 선언관련 버그방지
+    if (formKey.currentState == null) {
+      return;
+    }
+    if (formKey.currentState!.validate()) {
+      print('savePressed!');
+      Navigator.pop(context);
+    } else {
+      print('not validated');
+    }
   }
 }
